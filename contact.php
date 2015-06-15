a4838bd4c7c0e480e9c99a88b7b5443b05686aba
S2WDevelopment <sherwinskthomas@live.com>
2015-06-14 22:29:53 -0500

    random changes

--- contact.php
+++ contact.php
@@ -1,18 +1,29 @@
 <?php 
+
 $name = $_POST['name'];
 $email = $_POST['email'];
+$topic = $_POST['topic'];
 $message = $_POST['message'];
-$subject = $_POST['subject'];
+$from = 'From:prg-equipment.com';
+$to = 'ttaylor@prg-equipment.com';
+$subject = 'Message from prg-equipment.com';
 
-$to = 'sherwinthomas0@gmail.com';
-$message = 'FROM: '.$name.' Email: '.$email.'Message: '.$message;
-$headers = 'From: youremail@domain.com' . "\r\n";
- 
-if (filter_var($email, FILTER_VALIDATE_EMAIL)) { // this line checks that we have a valid email address
-mail($to, $subject, $message, $headers); //This method sends the mail.
-echo "Your email was sent!"; // success message
-}else{
-echo "Invalid Email, please provide an correct email.";
+$body = "From: $name\n E-Mail: $email\n Topic: $topic\n Message: $message\n";
+
+if ($_POST['submit'])
+{
+	if (mail ($to, $subject, $body, $from))
+	{
+		echo '<p>Your message has been sent! You should hear back from us soon.</p>';
+	}
+	else
+	{
+		echo '<p>Something went wrong, please call us or try again later.</p>';
+	}
+}
+else
+{
+	echo '<p>Something went wrong, please call us or try again later.</p>';
 }
 
 ?>
---
+++ request_forklift.php
@@ -0,0 +1,51 @@
+<?php 
+
+$business_name = $_POST['business_name'];
+$contact_name = $_POST['contact_name'];
+$email = $_POST['email'];
+$phone = $_POST['phone'];
+$lifting_capacity = $_POST['lifting_capacity'];
+$LPG = $_POST['LPG'];
+$diesel = $_POST['diesel'];
+$electric = $_POST['electric'];
+$stage3 = $_POST['stage3'];
+$stage2 = $_POST['stage2'];
+$cushion = $_POST['cushion'];
+$pneumatic = $_POST['pneumatic'];
+$fork_length = $_POST['fork_length'];
+$attachments = $_POST['attachments'];
+$other_reqs = $_POST['other_reqs'];
+
+$from = 'From:prg-equipment.com';
+$to = 'ttaylor@prg-equipment.com';
+$subject = 'Forklift Reqest Form from prg-equipment.com';
+
+$body = "From: $business_name\n
+ Contact Name: $contact_name\n
+ E-Mail: $email\n
+ Phone: $phone\n
+ Lifting Capacity Requested: $lifting_capacity\n
+ Fuel Type: LPG - $LPG, Diesel - $diesel, Electric - $electric\n
+ Mast Specification: 3 Stage - $stage3, 2 Stage - $stage2\n
+ Forklift Type: Cushion - $cushion, Pneumatic - $pneumatic\n
+ Fork Length: $fork_length\n
+ Forklift Attachments: $attachments\n
+ Other Requirements: $other_reqs\n";
+
+if ($_POST['submit'])
+{
+	if (mail ($to, $subject, $body, $from))
+	{
+		echo '<p>Your message has been sent! You should hear back from us soon.</p>';
+	}
+	else
+	{
+		echo '<p>Something went wrong, please call us or try again later.</p>';
+	}
+}
+else
+{
+	echo '<p>Something went wrong, please call us or try again later.</p>';
+}
+
+?>
